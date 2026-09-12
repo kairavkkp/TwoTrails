d622639726c27ebdb1aface0567e895b1b62e293
kairavkkp <kairavpithadia13@gmail.com>
2026-09-12 12:32:17 +0530

    Initial Commit

---
+++ TwoTrails/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TwoTrails
+//
+//  Created by Kairav Pithadia on 12/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TwoTrails/TwoTrailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoTrailsApp.swift
+//  TwoTrails
+//
+//  Created by Kairav Pithadia on 12/09/26.
+//
+
+import SwiftUI
+
+@main
+struct TwoTrailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
